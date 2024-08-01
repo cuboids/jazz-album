04fc52c973eccbf3ce32b5007b58ae6aa81849b4
cuboids <44651818+cuboids@users.noreply.github.com>
2024-08-01 19:09:55 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+import jazz_album_of_the_day
+
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for the Flask app
+
+
+@app.route('/', methods=['GET'])
+def get_jazz_album():
+
+    jaotd = jazz_album_of_the_day.main()
+
+    return render_template(
+        'index.html',
+        youtube_url=jaotd['youtube_url'],
+        album=jaotd['album'],
+        artist=jaotd['artist'])
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
