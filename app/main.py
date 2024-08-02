e6a7dd10992c1053ce0bde36a8be6ead111c98c5
cuboids <44651818+cuboids@users.noreply.github.com>
2024-08-02 13:22:38 +0200

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify, render_template
+import app.ja as ja
+
+bp = Blueprint('main', __name__)
+
+@bp.route('/', methods=['GET'])
+def get_jazz_album():
+    jaotd = ja.main()
+    return render_template(
+        'index.html',
+        youtube_url=jaotd['youtube_url'],
+        album=jaotd['album'],
+        artist=jaotd['artist']
+    )
