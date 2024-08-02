44f0f15480c16153b6250504739c807fe12d5094
cuboids <44651818+cuboids@users.noreply.github.com>
2024-08-02 13:21:49 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
+import os
+from flask import Flask
+from flask_cors import CORS
 
+def create_app():
+    app = Flask(__name__)
+    CORS(app)  # Enable CORS for the Flask app
+
+    with app.app_context():
+        from . import main
+        app.register_blueprint(main.bp)
+
+    return app
